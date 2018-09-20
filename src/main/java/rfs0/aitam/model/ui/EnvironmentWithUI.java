629e00036347bd24b08a78779b28afdf80eed982
RFS-0 <remo@mountain-yard.ch>
2018-09-20 10:28:38 +0200

    Allow object to have individual portrayals and refactoring

--- src/main/java/rfs0/aitam/model/ui/EnvironmentWithUI.java
+++ src/main/java/rfs0/aitam/model/ui/EnvironmentWithUI.java
@@ -15,7 +15,6 @@ import sim.display.GUIState;
 import sim.engine.SimState;
 import sim.portrayal.geo.GeomPortrayal;
 import sim.portrayal.geo.GeomVectorFieldPortrayal;
-import sim.util.geo.MasonGeometry;
 
 public class EnvironmentWithUI extends GUIState {
 
@@ -25,8 +24,6 @@ public class EnvironmentWithUI extends GUIState {
 	private GeomVectorFieldPortrayal m_buildingsPortrayal = new GeomVectorFieldPortrayal();
 	private GeomVectorFieldPortrayal m_pedestrianPathsPortrayal = new GeomVectorFieldPortrayal();
 	private GeomVectorFieldPortrayal m_agentPortrayal = new GeomVectorFieldPortrayal();
-	private GeomVectorFieldPortrayal m_homePortrayal = new GeomVectorFieldPortrayal(); // TODO: only for dev; remove later
-	private GeomVectorFieldPortrayal m_workPortrayal = new GeomVectorFieldPortrayal(); // TODO: only for dev; remove later
 
 	public EnvironmentWithUI(SimState state) {
 		super(state);
@@ -40,13 +37,10 @@ public class EnvironmentWithUI extends GUIState {
 	public void init(Controller controller) {
 		super.init(controller);
 		m_display = new Display2D(Environment.WIDTH, Environment.HEIGHT, this);
-
+		m_display.setMouseChangesOffset(true);
 		m_display.attach(m_buildingsPortrayal, "Buildings", true);
 		m_display.attach(m_pedestrianPathsPortrayal, "Pedestrian Paths", true);
 		m_display.attach(m_agentPortrayal, "Agents", true);
-		m_display.attach(m_homePortrayal, "Home", true);
-		m_display.attach(m_workPortrayal, "Work", true);
-		
 
 		m_displayFrame = m_display.createFrame();
 		controller.registerFrame(m_displayFrame);
@@ -57,25 +51,23 @@ public class EnvironmentWithUI extends GUIState {
 	public void start() {
 		super.start();
 		setupPortrayals();
-	}
-
-	private void setupPortrayals() {
-		Environment world = (Environment) state;
-
-		m_buildingsPortrayal.setField(world.m_buildings);
-		BuildingLabelPortrayal blP = new BuildingLabelPortrayal(new GeomPortrayal(Color.BLUE, 5.0), Color.DARK_GRAY);
-		m_buildingsPortrayal.setPortrayalForAll(blP);
-		m_buildingsPortrayal.setPortrayalForObject(world.m_buildings.getObjectsWithinDistance((MasonGeometry) world.m_buildings.getGeometries().get(0), 100000), new GeomPortrayal(Color.YELLOW, 100.0, true));
-
-		m_pedestrianPathsPortrayal.setField(world.m_paths);
-		m_pedestrianPathsPortrayal.setPortrayalForAll(new GeomPortrayal(Color.GRAY, true));
-
-		m_agentPortrayal.setField(world.m_individuals);
-		m_agentPortrayal.setPortrayalForAll(new GeomPortrayal(Color.RED, 15.0, true));
-		
 		m_display.reset();
 		m_display.setBackdrop(Color.WHITE);
 		m_display.repaint();
+	}
+
+	private void setupPortrayals() {
+		Environment environment = (Environment) state;
+
+		m_buildingsPortrayal.setField(environment.m_buildings);
+		BuildingLabelPortrayal blP = new BuildingLabelPortrayal(new GeomPortrayal(new Color(30,144,255), 5.0), Color.DARK_GRAY);
+		m_buildingsPortrayal.setPortrayalForRemainder(blP);
+
+		m_pedestrianPathsPortrayal.setField(environment.m_paths);
+		m_pedestrianPathsPortrayal.setPortrayalForRemainder(new GeomPortrayal(Color.LIGHT_GRAY, true));
+
+		m_agentPortrayal.setField(environment.m_individuals);
+		m_agentPortrayal.setPortrayalForAll(new GeomPortrayal(new Color(255,127,80), 5.0, true));
 	}
 
 	public static void main(String[] args) {
