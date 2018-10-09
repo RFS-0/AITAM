312f48029c4f9e0c0b1c115ec199132ebd4ad9bd
RFS-0 <remo@mountain-yard.ch>
2018-10-09 11:20:27 +0200

    update actual need time split

--- src/main/java/rfs0/aitam/model/needs/ActualNeedTimeSplit.java
+++ src/main/java/rfs0/aitam/model/needs/ActualNeedTimeSplit.java
@@ -3,20 +3,41 @@ package rfs0.aitam.model.needs;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import rfs0.aitam.utilities.CalculationUtility;
+
 public class ActualNeedTimeSplit {
 	
-	private HashMap<Need, BigDecimal> m_actualNeedTimeSplit = new HashMap<>();
+	private HashMap<Need, BigDecimal> m_absoluteNeedTimeSplit = new HashMap<>();
+	private boolean m_isUpdated = false;
+	private HashMap<Need, BigDecimal> m_relativeNeedTimeSplit = new HashMap<>();
 	
 	public ActualNeedTimeSplit() {}
 	
-	public HashMap<Need, BigDecimal> getActualNeedTimeSplit() {
-		return m_actualNeedTimeSplit;
+	public HashMap<Need, BigDecimal> getAbsoluteNeedTimeSplit() {
+		return m_absoluteNeedTimeSplit;
 	}
 	
 	public void updateNeedTimeSplit(Need need, BigDecimal timeSpentSatisfyingNeed) {
-		if (m_actualNeedTimeSplit.get(need) == null) {
-			m_actualNeedTimeSplit.put(need, timeSpentSatisfyingNeed);
+		if (m_absoluteNeedTimeSplit.get(need) == null) {
+			m_absoluteNeedTimeSplit.put(need, timeSpentSatisfyingNeed);
 		}
-		m_actualNeedTimeSplit.put(need, m_actualNeedTimeSplit.get(need).add(timeSpentSatisfyingNeed));
+		else {
+			m_absoluteNeedTimeSplit.put(need, m_absoluteNeedTimeSplit.get(need).add(timeSpentSatisfyingNeed));
+		}
+		m_isUpdated = false;
+	}
+	
+	public HashMap<Need, BigDecimal> getRelativeNeedTimeSplit() {
+		if (m_isUpdated) {
+			return m_relativeNeedTimeSplit;
+		}
+		else {
+			BigDecimal totalTimeSpentOnAllNeeds = CalculationUtility.sum(m_absoluteNeedTimeSplit.values());
+			for (Need need: m_absoluteNeedTimeSplit.keySet()) {
+				BigDecimal totalTimeSpentOnNeed = m_absoluteNeedTimeSplit.get(need);
+				m_relativeNeedTimeSplit.put(need, CalculationUtility.divide(totalTimeSpentOnNeed, totalTimeSpentOnAllNeeds));
+			}
+			return m_relativeNeedTimeSplit;
+		}
 	}
 }
