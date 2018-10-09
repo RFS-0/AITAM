7b2bc7b19cb1d15c1d38d99de12f78255320cf4e
RFS-0 <remo@mountain-yard.ch>
2018-10-09 11:12:51 +0200

    test step works as intended

---
+++ src/test/java/rfs0/aitam/model/EnvironmentTest.java
@@ -0,0 +1,20 @@
+package rfs0.aitam.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import rfs0.aitam.commons.ISimulationSettings;
+
+public class EnvironmentTest {
+
+	@Test
+	public void testStepEnvironment() {
+		Environment environment = new Environment(1L);
+		environment.start();
+		assertEquals(true, environment.schedule.step(environment));
+		DateTime timeAfterFirstStep = new DateTime(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY, ISimulationSettings.BASE_HOUR, 1);
+		assertEquals(environment.getSimulationTime().getCurrentTime(), timeAfterFirstStep);
+	}
+}
