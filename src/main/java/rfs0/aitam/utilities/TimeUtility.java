e70d78888a3492d6a83245c92b93936b6d4ca86c
RFS-0 <remo@mountain-yard.ch>
2018-10-15 11:00:09 +0200

    refactor time related functions to utility

--- src/main/java/rfs0/aitam/utilities/TimeUtility.java
+++ src/main/java/rfs0/aitam/utilities/TimeUtility.java
@@ -2,12 +2,16 @@ package rfs0.aitam.utilities;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.stream.Collectors;
 
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
+import activities.ActivityAgenda;
+import individuals.Individual;
 import rfs0.aitam.commons.ISimulationSettings;
+import rfs0.aitam.model.Environment;
 
 public final class TimeUtility {
 	
@@ -35,5 +39,106 @@ public final class TimeUtility {
 	
 	public static DateTime getStartOfNextDay(DateTime day) {
 		return day.plusDays(1).withHourOfDay(ISimulationSettings.BASE_HOUR).withMinuteOfHour(ISimulationSettings.BASE_MINUTE);
+	}
+	
+	public static boolean isIntervalOverlappingAnyAgenda(ArrayList<Individual> networkMembersParticipating, Interval intervalOfInterest) {
+		for (Individual individual: networkMembersParticipating) {
+			for (Interval plannedInterval: individual.getJointActivityAgenda().getIntervals()) {
+				if (plannedInterval.overlaps(intervalOfInterest)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isDayFullyPlanned(Environment environment, ActivityAgenda randomAgenda) {
+		DateTime currentTime = environment.getSimulationTime().getCurrentDateTime();
+		DateTime endOfCurrentDay = TimeUtility.getEndOfCurrentDay(currentTime);
+		if (randomAgenda.getFirstPlannedInterval() == null || !randomAgenda.getFirstPlannedInterval().getStart().equals(TimeUtility.getStartOfCurrentDay(currentTime))) {
+			return false;
+		}
+		if (!randomAgenda.getLastPlannedActivityInterval().getEnd().equals(TimeUtility.getEndOfCurrentDay(currentTime))) {
+			return false;
+		}
+		Interval preceding = null;
+		Interval latter = null;
+		Iterator<Interval> intervalIterator = randomAgenda.getIntervals().iterator();
+		if (!intervalIterator.hasNext()) {
+			return false;
+		}
+		else {
+			preceding = intervalIterator.next();
+		}
+		if (preceding.getEnd().equals(endOfCurrentDay)) {
+			return true;
+		}
+		else if (!intervalIterator.hasNext()) {
+			return false;
+		}
+		else {
+			latter = intervalIterator.next();
+		}
+		do {
+			if (!latter.abuts(preceding)) {
+				return false;
+			}
+			if (!intervalIterator.hasNext()) {
+				if (!latter.getEnd().equals(endOfCurrentDay)) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+			else {
+				preceding = latter;
+				latter = intervalIterator.next();
+			}
+		}
+		while (intervalIterator.hasNext());
+		return true;
+	}
+	
+	public static Interval getFirstAvailableInterval(Environment environment, ActivityAgenda agenda) {
+		DateTime currentTime = environment.getSimulationTime().getCurrentDateTime();
+		DateTime endOfCurrentDay = TimeUtility.getEndOfCurrentDay(currentTime);
+		Interval preceding = null;
+		Interval latter = null;
+		Iterator<Interval> intervalIterator = agenda.getIntervals().iterator();
+		if (!intervalIterator.hasNext()) {
+			return new Interval(currentTime, endOfCurrentDay);
+		}
+		else {
+			preceding = intervalIterator.next();
+			if (!preceding.getStart().equals(currentTime)) {
+				return new Interval(currentTime, preceding.getStart());
+			}
+		}
+		if (!intervalIterator.hasNext()) {
+			return new Interval(preceding.getEnd(), endOfCurrentDay);
+		}
+
+		do {
+			latter = intervalIterator.next();
+			if (!latter.abuts(preceding)) {
+				if (preceding.getEnd().isAfter(latter.getStart())) {
+					System.out.println(preceding);
+					System.out.println(latter);
+				}
+				return new Interval(preceding.getEnd(), latter.getStart());
+			}
+			else if (!intervalIterator.hasNext()) {
+				if (latter.getEnd().equals(endOfCurrentDay)) {
+					return null;
+				}
+				return new Interval(latter.getEnd(), endOfCurrentDay);
+			}
+			else {
+				preceding = latter;
+			}
+		} 
+		while (intervalIterator.hasNext());
+		return null;
 	}
 }
