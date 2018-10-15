7d0253dac8fddc4a1b628532d62834c1085083d7
RFS-0 <remo@mountain-yard.ch>
2018-10-15 10:58:48 +0200

    allow to retrieve time only (for base day)

--- src/main/java/rfs0/aitam/model/SimulationTime.java
+++ src/main/java/rfs0/aitam/model/SimulationTime.java
@@ -10,18 +10,22 @@ public class SimulationTime implements Steppable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private DateTime m_time = new DateTime(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY, ISimulationSettings.BASE_HOUR, ISimulationSettings.BASE_MINUTE);
+	private DateTime m_dateTime = new DateTime(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY, ISimulationSettings.BASE_HOUR, ISimulationSettings.BASE_MINUTE);
 
 	@Override
 	public void step(SimState state) {
-		m_time = m_time.plusMinutes(1);
+		m_dateTime = m_dateTime.plusMinutes(1);
+	}
+	
+	public DateTime getCurrentDateTime() {
+		return m_dateTime;
 	}
 	
 	public DateTime getCurrentTime() {
-		return m_time;
+		return m_dateTime.withDate(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY);
 	}
 
 	public int getCurrentWeekDay() {
-		return m_time.getDayOfWeek();
+		return m_dateTime.getDayOfWeek();
 	}
 }
