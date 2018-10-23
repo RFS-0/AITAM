9b639fb874ef210dd38ccdbed8614e533942fb46
RFS-0 <remo@mountain-yard.ch>
2018-10-23 15:43:54 +0200

    print info about what has been simulated yet

--- src/main/java/rfs0/aitam/model/SimulationTime.java
+++ src/main/java/rfs0/aitam/model/SimulationTime.java
@@ -1,6 +1,8 @@
 package rfs0.aitam.model;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import rfs0.aitam.commons.ISimulationSettings;
 import sim.engine.SimState;
@@ -9,6 +11,7 @@ import sim.engine.Steppable;
 public class SimulationTime implements Steppable {
 
 	private static final long serialVersionUID = 1L;
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.longDateTime();
 	
 	private DateTime m_dateTime = new DateTime(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY, ISimulationSettings.BASE_HOUR, ISimulationSettings.BASE_MINUTE);
 
@@ -16,6 +19,9 @@ public class SimulationTime implements Steppable {
 	public void step(SimState state) {
 		((Environment) state).getOutputHolder().put(ISimulationSettings.TIME_STAMP, m_dateTime); // record current value
 		m_dateTime = m_dateTime.plusMinutes(1); // increment time
+		if (m_dateTime.getHourOfDay() == 23 && m_dateTime.getMinuteOfHour() == 59) {
+			System.out.println("Simulated: " + DATE_TIME_FORMATTER.print(m_dateTime));
+		}
 	}
 	
 	public DateTime getCurrentDateTime() {
