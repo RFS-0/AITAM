8eb2ee9efcbb36cdf54362890ecd9a502b7abc3a
RFS-0 <remo@mountain-yard.ch>
2018-11-04 00:01:44 +0100

    refactor and document

--- src/main/java/rfs0/aitam/activities/ActivityCategory.java
+++ src/main/java/rfs0/aitam/activities/ActivityCategory.java
@@ -1,5 +1,9 @@
 package rfs0.aitam.activities;
 
+/**
+ * This enumeration contains all categories of activities. Its primary purpose is to group activities. Those groups
+ * are then used to facilitate the selection of relevant activities in different places of the model.
+ */
 public enum ActivityCategory {
 	LEISURE,
 	WORK,
