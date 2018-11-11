fdcad18a09391357969a472193b638f41210229c
RFS-0 <remo@mountain-yard.ch>
2018-11-11 22:00:18 +0100

    refactor and document

--- src/main/java/rfs0/aitam/model/SimulationTime.java
+++ src/main/java/rfs0/aitam/model/SimulationTime.java
@@ -8,13 +8,29 @@ import rfs0.aitam.settings.ISimulationSettings;
 import sim.engine.SimState;
 import sim.engine.Steppable;
 
+/**
+ * <p>This class is used to model and keep track of the simulation's time.
+ * To do so it relies on the following attributes:</p>
+ *
+ * <p>{@link SimulationTime#DATE_TIME_FORMATTER}: The formatter used to print the end of each simulation day in human readable form.</p>
+ * <p>{@link SimulationTime#m_dateTime}: The current time in the simulation. It is incremented each step by one minute (i.e. each step in the simulation corresponds to one minute in real time).</p>
+ */
 public class SimulationTime implements Steppable {
 
 	private static final long serialVersionUID = 1L;
-	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.longDateTime();
 	
+	/**
+	 * <p>The formatter used to print the end of each simulation day in human readable form.</p>
+	 */
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.longDateTime();
+	/**
+	 * <p>The current time in the simulation. It is incremented each step by one minute (i.e. each step in the simulation corresponds to one minute in real time).</p>
+	 */
 	private DateTime m_dateTime = new DateTime(ISimulationSettings.BASE_YEAR, ISimulationSettings.BASE_MONTH, ISimulationSettings.BASE_DAY, ISimulationSettings.BASE_HOUR, ISimulationSettings.BASE_MINUTE);
 
+	/**
+	 * <p>This method advances simulation time by one minute each time it is called (once per simulation step) and print the current date at the end of each day.</p>
+	 */
 	@Override
 	public void step(SimState state) {
 		((Environment) state).getOutputHolder().put(ISimulationSettings.TIME_STAMP, m_dateTime); // record current value
@@ -24,6 +40,9 @@ public class SimulationTime implements Steppable {
 		}
 	}
 	
+	/**
+	 * @category Getters
+	 */
 	public DateTime getCurrentDateTime() {
 		return m_dateTime;
 	}
