458fc76dff0dd14111aca97285723f88dafbed13
RFS-0 <remo@mountain-yard.ch>
2018-11-12 20:41:42 +0100

    document

--- src/test/java/rfs0/aitam/needs/NeedTimeSplitTest.java
+++ src/test/java/rfs0/aitam/needs/NeedTimeSplitTest.java
@@ -7,8 +7,8 @@ import java.math.BigDecimal;
 import org.junit.Test;
 
 import rfs0.aitam.model.needs.Need;
-import rfs0.aitam.model.needs.NeedTimeSplit;
-import rfs0.aitam.model.needs.NeedTimeSplit.Builder;
+import rfs0.aitam.model.needs.TargetNeedTimeSplit;
+import rfs0.aitam.model.needs.TargetNeedTimeSplit.Builder;
 import rfs0.aitam.settings.ISimulationSettings;
 import rfs0.aitam.utilities.CalculationUtility;
 
@@ -16,21 +16,21 @@ public class NeedTimeSplitTest {
 
 	@Test
 	public void testEquateNeedTimeSplit() {
-		Builder builder = new NeedTimeSplit.Builder();
+		Builder builder = new TargetNeedTimeSplit.Builder();
 		BigDecimal value = CalculationUtility.createBigDecimal(0.1);
-		NeedTimeSplit needTimeSplit = builder
+		TargetNeedTimeSplit needTimeSplit = builder
 				.withNeedTimeSplit(Need.AFFECTION, value)
 				.withNeedTimeSplit(Need.CREATION, value)
 				.withNeedTimeSplit(Need.FREEDOM, value)
 				.withNeedTimeSplit(Need.IDENTITY, value)
 				.build();
-		assertEquals(CalculationUtility.createBigDecimal(0.6), needTimeSplit.getNeedTimeSplit().get(Need.NOT_DEFINED));
+		assertEquals(CalculationUtility.createBigDecimal(0.6), needTimeSplit.getNeedTimeSplit().get(Need.NONE));
 		needTimeSplit = builder
 				.withNeedTimeSplit(Need.AFFECTION, BigDecimal.ONE)
 				.withNeedTimeSplit(Need.CREATION, value)
 				.withNeedTimeSplit(Need.FREEDOM, value)
 				.withNeedTimeSplit(Need.IDENTITY, value)
 				.build();
-		assertEquals(CalculationUtility.createBigDecimal(-0.3).setScale(ISimulationSettings.SCALE_USED_FOR_BIG_DECIMAL - 1), needTimeSplit.getNeedTimeSplit().get(Need.NOT_DEFINED));
+		assertEquals(CalculationUtility.createBigDecimal(-0.3).setScale(ISimulationSettings.SCALE_USED_FOR_BIG_DECIMAL - 1), needTimeSplit.getNeedTimeSplit().get(Need.NONE));
 	}
 }
