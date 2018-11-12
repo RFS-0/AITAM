a737391ebe8dc142dc60c3cc5a9afefd3d57f6a0
RFS-0 <remo@mountain-yard.ch>
2018-11-12 20:41:17 +0100

    document

--- src/main/java/rfs0/aitam/utilities/GeometryUtility.java
+++ src/main/java/rfs0/aitam/utilities/GeometryUtility.java
@@ -8,12 +8,22 @@ import rfs0.aitam.model.Environment;
 import sim.field.geo.GeomVectorField;
 import sim.util.geo.MasonGeometry;
 
+/**
+ * <p>This class is used to handle operations related to geometric objects.</p>
+ */
 public final class GeometryUtility {
 	
 	public static double calculateDistance(Coordinate c1, Coordinate c2) {
 		return c1.distance(c2);
 	}
 
+	/**
+	 * <p>This method retrieves all geometries of a field that cover the provided geometry.</p>
+	 * 
+	 * @param geometryCovered - the geometry that is covered by some field.
+	 * @param coveringField - the field that covers the provided geometry.
+	 * @return ArrayList<MasonGeometry> - a list with all object of the field that cover the geometry.
+	 */
 	public static ArrayList<MasonGeometry> getCoveringObjects(MasonGeometry geometryCovered, GeomVectorField coveringField) {
 		ArrayList<MasonGeometry> coveringMasonGeometries = new ArrayList<>();
 		for (Object masonGeometry: coveringField.getCoveringObjects(Environment.GEO_FACTORY.createPoint(geometryCovered.geometry.getCoordinate()))) {
