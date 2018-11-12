e790ea6e8e952dadd0fa5c466f144b4879d3f2c3
RFS-0 <remo@mountain-yard.ch>
2018-11-12 20:40:05 +0100

    document

--- src/main/java/rfs0/aitam/model/needs/Need.java
+++ src/main/java/rfs0/aitam/model/needs/Need.java
@@ -1,5 +1,9 @@
 package rfs0.aitam.model.needs;
 
+/**
+ * This enumeration contains a set of needs introduced by Manfred Max-Neef (see <a href="https://en.wikipedia.org/wiki/Fundamental_human_needs">fundamental human needs</a> for more information).
+ * In addition to those needs it also contains a pseudo need (NONE) which represents time lost.</p>
+ */
 public enum Need {
 	SUBSISTENCE,
 	PROTECTION,
@@ -10,5 +14,5 @@ public enum Need {
 	CREATION,
 	IDENTITY,
 	FREEDOM,
-	NOT_DEFINED
+	NONE
 }
