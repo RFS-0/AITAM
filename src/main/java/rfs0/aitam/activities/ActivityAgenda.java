1262628c0249d869606fe26644173a85cb9f95bf
remo <remo@remos-mbp.fritz.box>
2018-12-29 16:51:03 +0100

    Refactor ActivtyAgenda

--- src/main/java/rfs0/aitam/activities/ActivityAgenda.java
+++ src/main/java/rfs0/aitam/activities/ActivityAgenda.java
@@ -34,6 +34,8 @@ import rfs0.aitam.model.needs.NeedTimeSplit;
  */
 public class ActivityAgenda implements Cloneable {
 	
+	private static final Logger LOG = Logger.getLogger(ActivityAgenda.class.getName());
+	
 	/**
 	 * <p>The actual agenda containing all the activities planned sorted by their starting points. 
 	 * The key is an interval describing what time of day each activity of the agenda is executed. 
@@ -153,7 +155,7 @@ public class ActivityAgenda implements Cloneable {
 	 */
 	public void addNodeForInterval(Interval activityInterval, Node targetNode) {
 		if (activityInterval == null || targetNode == null) {
-			Logger.getLogger(ActivityAgenda.class.getName()).log(Level.SEVERE, String.format("At least one argument is invalid: activityInterval=%s;  targetNode=%s. Can not add this, since this would result in an invalid agenda.", Objects.toString(activityInterval), Objects.toString(targetNode)));
+			LOG.log(Level.SEVERE, String.format("At least one argument is invalid: activityInterval=%s;  targetNode=%s. Can not add this, since this would result in an invalid agenda.", Objects.toString(activityInterval), Objects.toString(targetNode)));
 		}
 		m_locations.put(activityInterval, targetNode);
 	}
@@ -198,7 +200,7 @@ public class ActivityAgenda implements Cloneable {
 	 */
 	public Set<Interval> getIntervals() {
 		if (!m_agenda.keySet().equals(m_locations.keySet())) {
-			Logger.getLogger(ActivityAgenda.class.getName()).log(Level.SEVERE, "Intervals for activities and locations are not identical. This agenda is invalid!");
+			LOG.log(Level.SEVERE, "Intervals for activities and locations are not identical. This agenda is invalid!");
 		}
 		return m_agenda.keySet();
 	}
